edd1da84acd26a6731bab8ad1c957687a26939e3
cstegall <36042238+cstegall@users.noreply.github.com>
2018-02-01 09:43:04 -0500

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,11 @@
+<link rel="stylesheet" type="text/css" href="mystylesheet.css"/>
+<div id="menu">
+<ul>
+	<li><a href="cstegall_home.html">Chanin Stegall IT Professional Toolkit</a></li>
+	<li><a href="policy.html">IT Policy</a></li>
+	<li><a href="strategy.html">IT Strategy</a></li>
+	<li><a href="governance.html">IT Governance</a></li>
+	<li><a href="glossary.html">Glossary</a></li>
+	<li><a href="references.html">References</a></li>
+</ul>
+</div>
